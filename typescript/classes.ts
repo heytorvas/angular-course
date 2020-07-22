14fb452fb36b33afb9937a134fb315a74f604731
Heytor Victor <heytorvas@gmail.com>
2020-07-22 17:34:38 -0300

    update

---
+++ typescript/classes.ts
@@ -0,0 +1,11 @@
+import {Spacecraft, Containership} from './base-ships'
+import {MilleniumFalcon} from './starfighters'
+
+let ship = new Spacecraft('hyperdrive')
+ship.jumpIntoHyperspace()
+
+let falcon = new MilleniumFalcon()
+falcon.jumpIntoHyperspace()
+
+let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2
+console.log(`Is falcon good for the job? ${goodForTheJob(falcon)}`)
