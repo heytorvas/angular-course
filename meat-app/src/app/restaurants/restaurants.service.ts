cdfdffe40e2e70ec6213903b907b2d403e1a9e93
Heytor Victor <heytorvas@gmail.com>
2020-07-24 15:49:26 -0300

    update

--- meat-app/src/app/restaurants/restaurants.service.ts
+++ meat-app/src/app/restaurants/restaurants.service.ts
@@ -9,6 +9,7 @@ import { Restaurant } from "./restaurant/restaurant.model";
 
 import {MEAT_API} from '../app.api'
 import {ErrorHandler} from '../app.error-handler'
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -18,5 +19,23 @@ export class RestaurantsService {
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
+    }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurants(id: string): Observable<MenuItem[]>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
