3cabd49cd4caf3ad438a47a73c6ac97c3b8a330e
Heytor Victor <heytorvas@gmail.com>
2020-07-27 11:35:19 -0300

    update

--- meat-app/src/app/order/order.component.ts
+++ meat-app/src/app/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { OrderService } from './order.service';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 
 @Component({
   selector: 'mt-order',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  delivery: number = 5
+
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Cartão de Crédito', value: 'CRE'},
+    {label: 'Cartão Refeição', value: 'REF'}
+  ]
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number{
+    return this.orderService.itemsValue()
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.increaseQty(item)
+  }
+  
+  decreaseQty(item: CartItem){
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.orderService.remove(item)
   }
 
 }
